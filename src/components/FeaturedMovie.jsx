d0728e23f6e97da3faabfb925be01175058adcfc
VinicinBraga <vinicius@bragamatta.com.br>
2021-08-18 18:24:03 -0300

    Seguindo com projeto Streming - Componentes e CSS

---
+++ src/components/FeaturedMovie.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./FeaturedMovie.css";
+
+const FeaturedMovie = ({ item }) => {
+  return (
+    <section
+      className="featured"
+      style={{
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
+      }}
+    >
+      <div>{item.original_name}</div>
+    </section>
+  );
+};
+
+export default FeaturedMovie;
