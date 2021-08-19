037058382af7ea595764b9380a97b4cf7fb8c6d3
VinicinBraga <vinicius@bragamatta.com.br>
2021-08-19 17:57:04 -0300

    Criando Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Header.css";
 
-const Header = () => {
-  return <header>...</header>;
+const Header = ({ black }) => {
+  return (
+    <header className={black ? "black" : ""}>
+      <div className="header--logo">
+        <a href="/">
+          <img
+            src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png"
+            alt="Netflix"
+          />
+        </a>
+      </div>
+      <div className="header--user">
+        <a href="/">
+          <img
+            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            alt="usuario"
+          />
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
